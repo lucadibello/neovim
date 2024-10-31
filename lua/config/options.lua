dbbfd3e822313758ca621bd829d937e8b580e8fd
Luca Di Bello <lucadibello@proton.me>
2024-10-31 20:30:23 +0100

    Now vimtex uses zathura as viewer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
+--
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.vimtex_view_method = "zathura"
+vim.g.latex_view_general_viewer = "zathura"

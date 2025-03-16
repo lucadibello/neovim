e72f31aec49cafa20bb3f3db156e39f94c35738d
Luca Di Bello <lucadibello@proton.me>
2025-03-16 22:38:16 +0100

    HOTFIX: now lualine is actually minimal

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,14 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
-    options = {
-      icons_enabled = true,
-    },
     sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "filename" },
-      lualine_y = { "location" },
+      -- Hide clock component
       lualine_z = {},
+
+      -- Show only the location component
+      lualine_y = {
+        { "location", padding = { left = 0, right = 1 } },
+      },
     },
-    inactive_sections = {
-      lualine_a = {},
-      lualine_b = {},
-      lualine_c = { "filename" },
-      lualine_x = {},
-      lualine_y = {},
-      lualine_z = {},
-    },
-    extensions = {},
   },
 }

7ce07ff4b012ea37fe5eee0985ededda89b08d10
Luca Di Bello <lucadibello@proton.me>
2025-03-16 22:33:34 +0100

    Minimal lualine + unused plugin `toggleterm`

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,23 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
+    options = {
+      icons_enabled = true,
+    },
     sections = {
-      -- Hide clock component
+      lualine_a = { "mode" },
+      lualine_b = { "filename" },
+      lualine_y = { "location" },
       lualine_z = {},
     },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+    extensions = {},
   },
 }

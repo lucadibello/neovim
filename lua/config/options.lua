0fe4f8ab2529e901c565f1aa7da3bde41d491b10
Luca Di Bello <lucadibello@proton.me>
2025-04-19 17:08:48 +0200

    Removed keymaps + forgotten settings + package updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,16 +5,3 @@
 
 vim.g.vimtex_view_method = "zathura"
 vim.g.latex_view_general_viewer = "zathura"
-
--- Molten setup
-vim.g.molten_auto_open_output = false
-vim.g.molten_image_provider = "image.nvim"
-vim.g.molten_wrap_output = true
-vim.g.molten_virt_text_output = true
-vim.g.molten_virt_lines_off_by_1 = true
-vim.g.molten_output_win_max_height = 20
-
--- Kernel setup (optional but recommended)
-vim.g.molten_use_border_highlights = true
-vim.g.molten_auto_image_popup = true
-vim.g.molten_auto_open_html_in_browser = false
